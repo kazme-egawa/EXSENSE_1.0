6bd1b4bb643f10440de203c3526633c272004bb5
kazme-egawa <arnerican.f0otboy@gmail.com>
2017-10-31 17:18:13 +0900

    Start Project

--- EXSENSE_1.0/SecondViewController.swift
+++ EXSENSE_1.0/SecondViewController.swift
@@ -6,20 +6,33 @@
 //  Copyright © 2017年 江川主民. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class SecondViewController: UIViewController {
 
+    // Init ColorPicker with white
+    var selectedColor: UIColor = UIColor.white
+    
+    // IBOutlet for the ColorPicker
+    @IBOutlet var colorPicker: SwiftHSVColorPicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setup Color Picker
+        colorPicker.setViewColor(UIColor.white)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func getSelectedColor(_ sender: UIButton) {
+        print("\(UInt16(colorPicker.hue * 255)) \(UInt16(colorPicker.saturation * 255)) \(UInt16(colorPicker.brightness * 255))")
+    }
 
 }
 
---
+++ EXSENSE_1.0/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  EXSENSE_1.0
+//
+//  Created by 江川主民 on 2017/10/31.
+//  Copyright © 2017年 江川主民. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
