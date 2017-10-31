83e89bcafe2989df4eb3c80cf2a37dad6a685353
kazme-egawa <arnerican.f0otboy@gmail.com>
2017-10-31 16:06:44 +0900

    Initial Commit

---
+++ EXSENSE_1.0/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  EXSENSE_1.0
+//
+//  Created by 江川主民 on 2017/10/31.
+//  Copyright © 2017年 江川主民. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
